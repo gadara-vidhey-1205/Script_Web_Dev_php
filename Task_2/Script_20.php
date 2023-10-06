ca182495c98f2fdb95bb5fe568c56561c283c121
GADARA VIDHEY DAMJIBHAI <143687409+gadara-vidhey-1205@users.noreply.github.com>
2023-10-06 22:24:35 +0530

    Script to find the factorial of a given number and display the result.

---
+++ Task_2/Script_20.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Script_19</title>
+    
+    <form action="" method="post">
+    Enter any number :<input type="number" name = 'num'>
+    <input type="submit" value="submit">
+    </form>
+</head>
+<body>
+    
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+<?php
+$number = $_POST['num'];
+for($i=1; $i<=$number; $i++){
+
+    echo $number*$i;
+}
+
+
+
+?>
