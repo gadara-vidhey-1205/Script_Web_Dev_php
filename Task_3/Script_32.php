b6a6df52384741d44d921cc7309b63b3c2f50962
GADARA VIDHEY DAMJIBHAI <143687409+gadara-vidhey-1205@users.noreply.github.com>
2023-10-26 10:11:34 +0530

    Write a PHP script to merge two arrays using the array_merge() function and display the combined result.

---
+++ Task_3/Script_32.php
@@ -0,0 +1,11 @@
+<?php
+
+$car1=array("VOLVO","BMW","MUSTANG");
+$car2=array("DODGE","GTR","BUGATTI");
+print_r($car1);
+echo "<br/>";
+print_r($car2);
+echo"<br/>";
+
+print_r(array_merge($car1,$car2));
+?>
