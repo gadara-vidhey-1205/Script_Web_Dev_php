1d3b81a0be1a1e6739eaf725efef9e5ce9aa77c9
GADARA VIDHEY DAMJIBHAI <143687409+gadara-vidhey-1205@users.noreply.github.com>
2023-10-26 10:15:11 +0530

    Develop a PHP script that uses the array_push() function to add elements to an array and the array_pop() function to remove elements from the end.

---
+++ Task_3/Script_37.php
@@ -0,0 +1,13 @@
+<?php
+
+$a=array("red","yellow","green");
+array_push($a,"blue","pink");
+print_r($a);
+
+echo "<br/>";
+
+$b=array("red","violet","white");
+array_pop($b);
+echo "<br/>";
+print_r($b);
+?>
