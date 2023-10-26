16eb0ec2e209dd64259af3ef1620e0ab9c04d3c9
GADARA VIDHEY DAMJIBHAI <143687409+gadara-vidhey-1205@users.noreply.github.com>
2023-10-26 11:13:53 +0530

    Create an associative array representing a person's details, including name, age, and email.

---
+++ Array Script/Script_12.php
@@ -0,0 +1,10 @@
+
+<?php
+//12. Create an associative array representing a person's details, including name, age, and email.
+
+$personal_detail = array(
+    "name"=>"Vidhey",
+    "age"=>"22",
+    "email"=>"vidheygadara123@gmail.com");
+print_r($personal_detail);
+?>
